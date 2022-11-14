34cafc650e8e999c9a254744d84772d9baa4808c
zv173769 <zv173769@gmail.com>
2022-11-14 10:36:09 +0800

    fix: CreatorToken success link

--- src/pages/Creator/CreatorToken/Review.tsx
+++ src/pages/Creator/CreatorToken/Review.tsx
@@ -5,7 +5,7 @@ import Pending from '../../../components/Pending'
 export default function Review({ hash }: { hash: string | undefined }) {
   return (
     <Box mt={60} display="flex" justifyContent={'center'}>
-      <Pending doneLink={routes.Tokens} doneText="Back to token list" hash={hash} />
+      <Pending doneLink={routes.Profile} doneText="To Profile" hash={hash} />
     </Box>
   )
 }
